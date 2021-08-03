740d5f4fb651736f607218bcad38546515b2c850
marcellpret <marcellpret@gmail.com>
2021-08-03 18:33:08 +0200

    First commit for Social Network

--- server/server.js
+++ server/server.js
@@ -2,10 +2,30 @@ const express = require("express");
 const app = express();
 const compression = require("compression");
 const path = require("path");
+const db = require("./db");
+const secrets = require("./secrets.json");
+const cookieSession = require("cookie-session");
+const encrypt = require("./encrypt");
+
+app.use(
+    cookieSession({
+        secret: process.env.COOKIE_SECRET || secrets.COOKIE_SECRET,
+        maxAge: 1000 * 60 * 60 * 24 * 14,
+        sameSite: true,
+    })
+);
+
+app.use(express.json());
 
 app.use(compression());
 
 app.use(express.static(path.join(__dirname, "..", "client", "public")));
+
+app.get("/user/id.json", function (req, res) {
+    res.json({
+        userId: req.session.userId,
+    });
+});
 
 app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "..", "client", "index.html"));
